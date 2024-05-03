1992ea4fde37b6d1eabc08295d31b0f2179f17c1
nhatanh2709 <127209960+nhatanh2709@users.noreply.github.com>
2024-05-03 21:23:42 +0700

    Update apiCalls.js

--- client/src/pages/authContext/apiCalls.js
+++ client/src/pages/authContext/apiCalls.js
@@ -5,7 +5,7 @@ export const login = async(user, dispatch) => {
     dispatch(loginStart());
     try {
         console.log(user);
-        const res = await axios.post("http://localhost:8800/api/auth/login", user);
+        const res = await axios.post(`${proccess.env.REACT_APP_URL}/api/auth/login`, user);
         
         dispatch(loginSuccess(res.data));
         console.log(res.data);
